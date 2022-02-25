6c8f38a89d3cf5ae7cf0fc961be87d06f47218d4
myla steiger <mrs.steiger@gmail.com>
2022-02-25 11:20:33 -0500

    Add project files.

---
+++ FutureValue/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using FutureValue.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FutureValue.Controllers {
+    public class HomeController : Controller {
+        [HttpGet]
+        public IActionResult Index() {
+            ViewBag.FV = 0;
+           return View();
+        }
+        [HttpPost]
+        public IActionResult Index(FutureValueModel model) {
+            ViewBag.FV = model.CalculateFutureValue();
+            return View(model);
+        }
+    }
+}
